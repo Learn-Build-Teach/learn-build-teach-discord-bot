ce48ecfc03b692a3612867a2e9665dc7de68cfed
James Quick <james.q.quick@outlook.com>
2020-12-16 08:47:55 -0600

    Added repsonded for share

--- Commands.js
+++ Commands.js
@@ -15,6 +15,7 @@ const share = async (msg) => {
         await airtable.create([
             { fields: { name: msg.author.username, link } },
         ]);
+        msg.reply(`Content successfully shared. Thanks ${msg.author}`);
     } catch (err) {
         console.error(err);
     }
