269721ba0a117e6c5b1213f3715808bfb9d6ebc9
James Quick <james.q.quick@outlook.com>
2021-01-06 19:53:01 -0600

    working on tweeting

--- tweeter.js
+++ tweeter.js
@@ -1,6 +1,18 @@
 const { sendTweet } = require('./utils/Twitter');
-
+const { shareTable } = require('./utils/Airtable');
 //setup cron job
 //poll airtable for links
 //tweet one and mark as shared
 //sendTweet('Test');
+
+const getNextTweetableShare = async () => {
+    const record = await shareTable
+        .select({
+            maxRecords: 1,
+            filterByFormula: `{tweetable} = "1"`,
+        })
+        .firstPage();
+    console.log(record);
+};
+
+getNextTweetableShare();
