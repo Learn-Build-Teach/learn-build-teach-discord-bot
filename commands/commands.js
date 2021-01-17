c6f800003e447acbbc5ebfa1e8ac6daa4eb1c695
James Quick <james.q.quick@outlook.com>
2021-01-16 22:03:08 -0600

    Added formatting with embeds. Saving profile information with user id as primary key. Added profile command to get info about a user

--- commands/commands.js
+++ commands/commands.js
@@ -1,20 +1,43 @@
+const Discord = require('discord.js');
+
 module.exports = {
     callback: async (msg) => {
-        return await msg.reply(
-            `
-**!updateProfile**: Update your profile with the following flags. I will use these pieces of information to help share your content!
+        const commandsEmbed = new Discord.MessageEmbed()
+            .setColor('#de5254')
+            .setTitle('LBT Bot Commands')
+            .addFields(
+                { name: '!updateProfile', value: updateProfileText },
+                { name: '!profile', value: profileText },
+                { name: '!share', value: shareText },
+                { name: '!commands', value: commandsText }
+            );
+        msg.channel.send(commandsEmbed);
+    },
+};
+
+const updateProfileText = `
+Update your profile with the following flags. I will use these pieces of information to help share your content.\n
     -twitter: your twitter handle
     -twitch: your Twitch username
     -youtube: your YouTube url
-    -website: your personal website
+    -website: your personal website\n
     ex. \`!updateProfile -twitter jamesqquick -youtube https://www.youtube.com/c/jamesqquick -website https://www.jamesqquick.com/\`
+    \n`;
 
-**!share**: Share a piece of content (article, video, etc.) by including a valid URL. I will consider sharing this on Twitter and/or my newsletter. You can (optionally) include a suggested tweet text after the URL.
+const profileText = `
+Get profile details about you or another discord member.
+    
+    ex. \`!profile\` for yourself or \`!profile @James Q Quick\` for someone else
+    \n`;
+
+const shareText = `
+Share a piece of content (article, video, etc.) by including a valid URL. I will consider sharing this on Twitter and/or my newsletter. You can (optionally) include a suggested tweet text after the URL.
+
     ex. \`!share https://www.jamesqquick.com/\ This is a suggested tweet text\`
+    \n`;
 
-**!commands**: Show a list of available commands
+const commandsText = `
+Show a list of available commands
+
     ex. \`!commands\`
-`
-        );
-    },
-};
+\n`;
---
+++ commands/profile.js
@@ -0,0 +1,67 @@
+const { userTable, minifyRecords } = require('../utils/Airtable');
+const Discord = require('discord.js');
+const commands = require('./commands');
+
+const getProfile = async (msg) => {
+    let targetUsername = msg.author.id;
+    const parts = msg.content.split(' ');
+    if (parts.length === 2) {
+        targetUsername = parts[1].replace('<@!', '').replace('>', '');
+    }
+    console.log(targetUsername);
+    console.log(msg.content);
+
+    try {
+        const records = minifyRecords(
+            await userTable
+                .select({
+                    maxRecords: 1,
+                    filterByFormula: `{discordId} = "${targetUsername}"`,
+                })
+                .firstPage()
+        );
+
+        if (records.length === 1) {
+            const user = records[0];
+            const commandsEmbed = new Discord.MessageEmbed()
+                .setColor('#de5254')
+                .setTitle('LBT Bot Commands');
+
+            user.fields.twitter &&
+                commandsEmbed.addFields({
+                    name: 'Twitter',
+                    value: user.fields.twitter || '',
+                });
+            user.fields.twitter &&
+                commandsEmbed.addFields({
+                    name: 'YouTube',
+                    value: user.fields.youtube || '',
+                });
+            user.fields.website &&
+                commandsEmbed.addFields({
+                    name: 'Website',
+                    value: user.fields.website || '',
+                });
+            user.fields.twitch &&
+                commandsEmbed.addFields({
+                    name: 'Twitch',
+                    value: user.fields.twitch || '',
+                });
+            msg.channel.send(commandsEmbed);
+        } else {
+            await msg.reply(`Couldn't find details on that user.`);
+        }
+    } catch (err) {
+        console.error(
+            `Something went wrong searching for user profile: ${targetUsername}.`
+        );
+        console.error(err);
+        return msg.channel.send(
+            `Something went wrong searching for user profile: ${targetUsername}.`
+        );
+    }
+};
+
+module.exports = {
+    callback: getProfile,
+};
