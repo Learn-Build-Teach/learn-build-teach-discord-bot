6a80c95cd82848ed31ebefc24f962eb1c0374788
James Quick <james.q.quick@outlook.com>
2021-03-01 16:09:05 -0600

    Handling new video command by getting embed info from Discord

--- commands/newVideo.js
+++ commands/newVideo.js
@@ -1,11 +1,72 @@
+const { shareTable } = require('../utils/Airtable');
+
 module.exports = {
     callback: async (msg) => {
-        //TODO:Check message comes from mee6 bot
-        //TODO: find metadata for video - link, image, creator
-        //TODO: add to share table in airtabl
-        console.log(msg);
-        console.log(msg.author);
-        console.log(msg.content);
-        console.log('Logging example YouTube video notification');
+        if (!msg.embeds || !msg.embeds[0]) {
+            return console.log(
+                'Ignoring !newVideo command since there is no embed data.'
+            );
+        }
+
+        const embed = msg.embeds[0];
+        console.log(`Attempting to share video`, embed);
+        const requiredProperties = ['author', 'thumbnail', 'provider'];
+        requiredProperties.forEach((prop) => {
+            if (!embed[prop]) {
+                return console.log(
+                    `Ignoring !newVideo command since there is no ${prop}.`
+                );
+            }
+        });
+
+        const {
+            url: link,
+            title,
+            description,
+            author: { name: discordUser },
+            thumbnail: { url: image },
+            provider: { name: provider },
+        } = embed;
+
+        if (provider !== 'YouTube') {
+            return console.log(
+                'Ignoring !newVideo command since the provider is not YouTube).'
+            );
+        }
+        if (!link) {
+            return console.log(
+                'Ignoring !newVideo command since there is no link'
+            );
+        }
+        if (!title) {
+            return console.log(
+                'Ignoring !newVideo command since there is no title'
+            );
+        }
+
+        try {
+            await shareTable.create([
+                {
+                    fields: {
+                        discordUser,
+                        link,
+                        title,
+                        ...(image && { image }),
+                        ...(description && { description }),
+                        tweetable: false,
+                    },
+                },
+            ]);
+            await msg.react(`🔥`);
+            await msg.reply(`This video has been shared to Airtable!`);
+        } catch (err) {
+            console.error(
+                'Something went wrong in sharing YouTube video to airtable.'
+            );
+            console.error(err);
+            await msg.reply(
+                `Failed to save share record 🤷‍♂️. <@361868131997843456> should take a look!`
+            );
+        }
     },
 };
