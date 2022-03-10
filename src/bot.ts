1d99d52abc5833ba81bac6ad4c4390297bc4ad81
James Q Quick <james.q.quick@gmail.com>
2022-03-10 15:43:26 -0600

    Logging on incoming command

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,10 @@
-import { KudoCategory } from '@prisma/client'
-import { Client, Intents, MessageReaction } from 'discord.js'
-import dotenv from 'dotenv'
-import fs from 'fs'
-import path from 'path'
-import { giveKudos } from './utils/db/kudos'
-dotenv.config()
+import { KudoCategory } from '@prisma/client';
+import { Client, Intents, MessageReaction } from 'discord.js';
+import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+import { giveKudos } from './utils/db/kudos';
+dotenv.config();
 const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
@@ -12,87 +12,88 @@ const client = new Client({
     Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
   ],
   partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-})
+});
 
-const nameToCommandMap: any = {}
+const nameToCommandMap: any = {};
 
 client.on('ready', async () => {
-  console.log('The Learn Build Teach bot is running!')
-  const guildId = process.env.DISCORD_GUILD_ID || ''
-  const guild = client.guilds.cache.get(guildId)
-  let commands: any
+  console.log('The Learn Build Teach bot is running!');
+  const guildId = process.env.DISCORD_GUILD_ID || '';
+  const guild = client.guilds.cache.get(guildId);
+  let commands: any;
 
   if (guild) {
-    commands = guild.commands
+    commands = guild.commands;
   } else {
-    commands = client.application?.commands
+    commands = client.application?.commands;
   }
-  const commandsDir = process.env.COMMANDS_DIR || 'commands'
-  const commandsFullPath = path.join(__dirname, commandsDir)
+  const commandsDir = process.env.COMMANDS_DIR || 'commands';
+  const commandsFullPath = path.join(__dirname, commandsDir);
 
   //TODO: handle errors
-  const commandFiles = fs.readdirSync(commandsFullPath)
+  const commandFiles = fs.readdirSync(commandsFullPath);
   const filePromises = commandFiles
     .filter(
       (commandFile) =>
         commandFile.endsWith('.js') || commandFile.endsWith('.ts')
     )
     .map((commandFile) => {
-      return import(`./commands/${commandFile}`)
-    })
-  const loadedFiles = await Promise.all(filePromises)
+      return import(`./commands/${commandFile}`);
+    });
+  const loadedFiles = await Promise.all(filePromises);
 
   loadedFiles.forEach((loadedFile) => {
-    const commandConfig = loadedFile.default
-    console.log('Loading command', commandConfig.name)
+    const commandConfig = loadedFile.default;
+    console.log('Loading command', commandConfig.name);
     try {
-      commands.create(commandConfig)
-      nameToCommandMap[commandConfig.name] = commandConfig
+      commands.create(commandConfig);
+      nameToCommandMap[commandConfig.name] = commandConfig;
     } catch (err) {
-      console.error(err)
-      return
+      console.error(err);
+      return;
     }
-  })
-})
+  });
+});
 
 client.on('messageReactionAdd', async (reaction, user) => {
-  if (!(reaction instanceof MessageReaction)) return
+  if (!(reaction instanceof MessageReaction)) return;
   if (reaction.message.partial) {
     // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
     try {
       await reaction.message.fetch();
     } catch (error) {
-      console.error('Something went wrong when fetching the message: ', error)
+      console.error('Something went wrong when fetching the message: ', error);
       // Return as `reaction.message.author` may be undefined/null
-      return
+      return;
     }
   }
-  const { emoji, message } = reaction
-  const { author: originalAuthor } = message
+  const { emoji, message } = reaction;
+  const { author: originalAuthor } = message;
 
-  if (!originalAuthor || !emoji?.name) return
+  if (!originalAuthor || !emoji?.name) return;
 
-  const emojisWeCareAbout = ['learn', 'build', 'teach']
+  const emojisWeCareAbout = ['learn', 'build', 'teach'];
   if (emojisWeCareAbout.includes(emoji.name || '')) {
     const category: KudoCategory =
-      KudoCategory[emoji.name.toUpperCase() as keyof typeof KudoCategory]
+      KudoCategory[emoji.name.toUpperCase() as keyof typeof KudoCategory];
     try {
-      const kudo = await giveKudos(user.id, originalAuthor.id, category)
-      console.info(`Kudo given`, kudo)
+      const kudo = await giveKudos(user.id, originalAuthor.id, category);
+      console.info(`Kudo given`, kudo);
     } catch (error) {
-      console.error('Well, something went wrong 🤷‍♂️')
+      console.error('Well, something went wrong 🤷‍♂️');
     }
   }
-})
+});
 
 client.on('interactionCreate', (interaction) => {
-  if (!interaction.isCommand()) return
+  if (!interaction.isCommand()) return;
 
-  const { commandName, options } = interaction
-  const existingCommand = nameToCommandMap[commandName]
+  const { commandName, options } = interaction;
+  const existingCommand = nameToCommandMap[commandName];
   if (existingCommand) {
-    return existingCommand.callback(interaction, options)
+    console.info(`Incoming command we care about: ${commandName}`);
+    return existingCommand.callback(interaction, options);
   }
-})
+});
 
-client.login(process.env.DISCORD_BOT_TOKEN)
+client.login(process.env.DISCORD_BOT_TOKEN);
