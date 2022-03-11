94b115e0a55b8517c8a198a4cb7fcb160cbc5687
James Q Quick <james.q.quick@gmail.com>
2022-03-11 09:56:29 -0600

    Feedback when trtying to get self kudos

--- src/commands/kudos.ts
+++ src/commands/kudos.ts
@@ -1,56 +1,59 @@
-import { KudoCategory, Prisma } from '.prisma/client'
+import { KudoCategory, Prisma } from '.prisma/client';
 import {
   CommandInteraction,
   CommandInteractionOptionResolver,
   GuildMember,
-} from 'discord.js'
-import { giveKudos } from '../utils/db/kudos'
+} from 'discord.js';
+import { giveKudos } from '../utils/db/kudos';
 
 const choices = Object.keys(KudoCategory).map((key) => {
   return {
     name: KudoCategory[key as keyof typeof KudoCategory],
     value: KudoCategory[key as keyof typeof KudoCategory],
-  }
-})
+  };
+});
 
 const handleKudos = async (
   interaction: CommandInteraction,
   options: CommandInteractionOptionResolver
 ) => {
   try {
-    const mentionedUser = options.getMentionable('user', false)
+    const mentionedUser = options.getMentionable('user', false);
 
     if (!(mentionedUser instanceof GuildMember)) {
       return interaction.reply({
         content: `You can only give kudos to a User 🥰`,
-      })
+      });
     }
 
-    const { id: receiverId } = mentionedUser
-    const { id: giverId } = interaction.user
+    const { id: receiverId } = mentionedUser;
+    const { id: giverId } = interaction.user;
 
     //If the giver and receiver are the same just abort
     //no need to let people kudo themselves I think
-    if (receiverId === giverId) return
+    if (receiverId === giverId)
+      return interaction.reply({
+        content: `Silly rabbit, you can't give kudos to yourself!`,
+      });
 
-    const description = options.getString('for', true)
-    const categoryString = options.getString('category', true)
+    const description = options.getString('for', true);
+    const categoryString = options.getString('category', true);
 
     //HACK: https://stackoverflow.com/questions/50417254/dynamically-access-enum-in-typescript-by-key
     const category: KudoCategory =
-      KudoCategory[categoryString as keyof typeof KudoCategory]
+      KudoCategory[categoryString as keyof typeof KudoCategory];
 
-    giveKudos(giverId, receiverId, category, description)
+    await giveKudos(giverId, receiverId, category, description);
     return interaction.reply({
       content: `${category} Kudos were given to ${mentionedUser.user.username} for '${description}'`,
-    })
+    });
   } catch (err) {
-    console.error(err)
+    console.error(err);
     return interaction.reply({
       content: `Something went wrong giving kudos :(`,
-    })
+    });
   }
-}
+};
 export default {
   callback: handleKudos,
   name: 'kudos',
@@ -76,4 +79,4 @@ export default {
       choices: choices,
     },
   ],
-}
+};
