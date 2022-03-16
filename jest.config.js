3eb0a464c2a8be3d01ac9148eae7a85d8c53e927
Mark Huggins <mhuggins@glgroup.com>
2022-03-15 22:28:01 -0400

    more env setup getting prepped for fixing the tests

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  moduleDirectories: ['node_modules', 'src'],
+};
